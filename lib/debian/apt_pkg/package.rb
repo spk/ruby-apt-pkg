a31ed1de5bddd0d6720ac6d492f81889429f7cc6
Laurent Arnoud <laurent@spkdev.net>
2020-07-24 10:00:00 +0200

    Switch debian/apt_pkg/package to rdoc

--- lib/debian/apt_pkg/package.rb
+++ lib/debian/apt_pkg/package.rb
@@ -10,22 +10,31 @@ module Debian
         :current_version
 
       # Initialize the Package class
-      # @example
-      #   new(id, name, full_name, arch, essential, important, current_version)
-      # @param [Integer] The numeric ID of the package
-      # @param [String] The name of the package
-      # @param [String] Get the full name of the package, including the
-      #                 architecture
-      # @param [String] The architecture of the package
-      # @param [Boolean] Boolean value determining whether the package is
-      #                  essential
-      # @param [Boolean] Boolean value determining whether the package has the
-      #                 'important' flag set
-      #                 ('Important: yes' in the Packages file)
-      # @param [Version,NilClass] The version of the package currently installed
-      #                           or `nil`
-      # @return [Package] Package instance
-      def initialize(id, name, full_name, arch, essential, important, current_version)
+      #
+      #   new(42, "ed", "ed:amd64", "amd64", false, false, nil)
+      #
+      # +id+:: [Integer] The numeric ID of the package
+      #
+      # +name+:: [String] The name of the package
+      #
+      # +full_name+:: [String] Get the full name of the package, including the
+      #                        architecture
+      #
+      # +arch+:: [String] The architecture of the package
+      #
+      # +essential+:: [Boolean] Boolean value determining whether the package is
+      #                         essential
+      #
+      # +important+:: [Boolean] Boolean value determining whether the package
+      #                         has the 'important' flag set ('Important: yes'
+      #                         in the Packages file)
+      #
+      # +current_version+:: [Version,NilClass] The version of the package
+      #                                        currently installed or nil
+      #
+      # Returns a Package instance
+      def initialize(id, name, full_name, arch, essential, important,
+                     current_version)
         @id = id
         @name = name
         @full_name = full_name
@@ -35,8 +44,11 @@ module Debian
         @current_version = current_version
       end
 
-      # Return `true` if the package is installed
-      # @return [Boolean]
+      # Return +true+ if the package is installed
+      #
+      #   Debian::AptPkg::Package.new(id, pkg_name, full_name, arch,
+      #                               essential, important,
+      #                               current_version).is_installed # => true
       def is_installed
         return false unless current_version
 
